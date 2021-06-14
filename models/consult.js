7b176f8d7baf3efc1cdac370feb506a747393a45
ehdcjf <kdch0823@naver.com>
2021-06-14 15:53:04 +0900

    db lag update

--- models/consult.js
+++ models/consult.js
@@ -39,6 +39,8 @@ module.exports = function(sequelize, DataTypes) {
     sequelize,
     tableName: 'consult',
     timestamps: false,
+    charset: "utf8mb4",
+    collate: "utf8mb4_general_ci",
     indexes: [
       {
         name: "PRIMARY",
