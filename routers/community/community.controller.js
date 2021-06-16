1e43b9b3e223f73c782d7504ce7fbc93ce3d7008
test <test@test.com>
2021-06-16 11:25:27 +0900

    slider and 수강후기 수정 페이지 작업

--- routers/community/community.controller.js
+++ routers/community/community.controller.js
@@ -1,8 +1,8 @@
-const { board,User } = require("../../models");
+const { board, User } = require("../../models");
 const jwtId = require('../../jwtId');
 const jwtName = require('../../jwtName');
 const article_count = 10;
-const {update_hit} = require('../update_hit'); 
+const { update_hit } = require('../update_hit');
 
 
 let boardType = {
@@ -15,7 +15,7 @@ let boardType = {
 
 let list = async (req, res) => {
     let { AccessToken } = req.cookies;
-    let {page} = req.query
+    let { page } = req.query
     let userid = (AccessToken != undefined) ? jwtId(AccessToken) : undefined;
     let username = (AccessToken != undefined) ? jwtName(AccessToken) : undefined;
     let board_name = req.params.board_name;
@@ -30,11 +30,11 @@ let list = async (req, res) => {
         include: [{
             model: User,
             as: 'writer_user',
-          }],
+        }],
         where: { type, },
     })
 
-  
+
 
     let count = await board.count({
         where: { type, },
@@ -50,7 +50,7 @@ let list = async (req, res) => {
     let block = 0;
     let p = 1;
     let nowblock = 0;
-    let nowpageblock; 
+    let nowpageblock;
     while (count > 0) {
         count -= article_count;
         pageblock[block].push(p)
@@ -85,7 +85,7 @@ let list = async (req, res) => {
 
 
     res.render(`./community/list`, {
-        result, title, board_name, userid, username, nowpageblock, start, end, prev, next,page
+        result, title, board_name, userid, username, nowpageblock, start, end, prev, next, page
     })
 }
 
@@ -93,10 +93,10 @@ let view = async (req, res) => {
     let { AccessToken } = req.cookies;
     let userid = (AccessToken != undefined) ? jwtId(AccessToken) : undefined;
     let username = (AccessToken != undefined) ? jwtName(AccessToken) : undefined;
-    let {id, num}  = req.query;
-    
-    const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-    update_hit(id,ip);
+    let { id, num } = req.query;
+
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    update_hit(id, ip);
     let page = req.query.page;
     let board_name = req.params.board_name;
     let title = boardType[board_name][0];
@@ -105,12 +105,12 @@ let view = async (req, res) => {
         include: [{
             model: User,
             as: 'writer_user',
-          }],
+        }],
         where: { id, },
     });
-    result['num']= num; 
+    result['num'] = num;
     res.render('./community/view', {
-        result, title, board_name, userid,page,
+        result, title, board_name, userid, page,
     })
 
 }
@@ -132,7 +132,7 @@ let review = async (req, res) => {
         include: [{
             model: User,
             as: 'writer_user',
-          }],
+        }],
         where: { type, },
     })
 
@@ -140,7 +140,7 @@ let review = async (req, res) => {
         where: { type, },
     });
 
- 
+
     let end = Math.ceil(count / article_count);
     let N = count - article_count * (page - 1);
     result = number_set(result, N);
@@ -150,7 +150,7 @@ let review = async (req, res) => {
     let block = 0;
     let p = 1;
     let nowblock = 0;
-    let nowpageblock; 
+    let nowpageblock;
     while (count > 0) {
         count -= article_count;
         pageblock[block].push(p)
@@ -179,7 +179,7 @@ let review = async (req, res) => {
     } else {
         next = pageblock[nowblock + 1][0];
     }
-    console.log(userid); 
+    //console.log(userid);
     res.render('./community/review', {
         userid,
         username,
@@ -215,34 +215,63 @@ let review_insert = async (req, res) => {
     })
 
     res.redirect(`/community/review_view?id=${result.id}&page=1`)
-    
+
 }
 
-let review_view = async(req, res) => {
+let review_view = async (req, res) => {
     // 여기서 DB에서 받아와서 값 뿌려주면 됨
     let { AccessToken } = req.cookies;
     let page = req.query.page;
-    let {id,num} = req.query;
-    const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-    update_hit(id,ip);
-    
+    let { id, num } = req.query;
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    update_hit(id, ip);
+
     let result = await board.findOne({
-        attributes: ['writer', 'subject', 'content', 'date', 'hit'],
+        attributes: ['id', 'writer', 'subject', 'content', 'date', 'hit'],
         include: [{
             model: User,
             as: 'writer_user',
-          }],
+        }],
         where: { id, },
     });
-    
     let userid = (AccessToken != undefined) ? jwtId(AccessToken) : undefined;
     let username = (AccessToken != undefined) ? jwtName(AccessToken) : undefined;
-    result['num']= num; 
+    result['num'] = num;
     res.render('./community/review_view', {
-       userid,username,page,result, 
+        userid, username, page, result
     });
 }
 
+
+
+let review_modify = async (req, res) => {
+    let { writer, id, page, num } = req.query;
+
+    let result = await User.findOne({
+        where: { username: writer }
+    })
+    let { userid } = result.dataValues;
+
+    let result2 = await board.findOne({
+        where: { writer: userid, id }
+    })
+    res.render('./community/review_modify', {
+        result2, writer, id ,page, num
+    });
+}
+
+
+let review_modify_submit = async (req, res) => {
+    let { id, page, num } = req.query;
+    let { review_title, review_writer, review_content } = req.body;
+
+    await board.update({
+        subject: review_title,
+        content: review_content
+    }, { where: { id } })
+
+    res.redirect(`/community/review_view?page=${page}&id=${id}&num=${num}`)
+}
 
 
 module.exports = {
@@ -252,6 +281,8 @@ module.exports = {
     review_write,
     review_insert,
     review_view,
+    review_modify,
+    review_modify_submit
 }
 
 //글번호생성 함수 
