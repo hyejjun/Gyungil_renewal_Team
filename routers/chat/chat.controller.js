a01e4a205e6039f4975063e39351043e0d444f2d
ehdcjf <kdch0823@naver.com>
2021-06-22 00:47:16 +0900

    chat update2

--- routers/chat/chat.controller.js
+++ routers/chat/chat.controller.js
@@ -1,54 +1,51 @@
 
-let {clients} = require('../admin/consult/consult.controller')
+let { clients } = require('../admin/consult/consult.controller')
 
-let getchat = (req,res)=>{ 
+let getchat = (req, res) => {
   res.render('./chat/chat.html')
 }
 
-let request = (req,res)=>{
-  console.log(req.body); 
-  let {id, name, age} = req.body; 
-  let now = new Date(); 
-  let mm = now.getMonth()+1;
+let request = (req, res) => {
+  let { id, name, age } = req.body;
+  let now = new Date();
+  let mm = now.getMonth() + 1;
   let d = now.getDate();
-  let h = now.getHours(); 
-  let m = now.getMinutes(); 
+  let h = now.getHours();
+  let m = now.getMinutes();
 
   now = `${mm}/${d}  ${h}:${m}`
-  let temp = {}; 
-  temp['id'] = id; 
-  temp['name'] = name; 
+  let temp = {};
+  temp['id'] = id;
+  temp['name'] = name;
   temp['age'] = age;
   temp['date'] = now;
-  temp['consultant'] = null; 
-  let flag=true; 
+  temp['consultant'] = null;
+  let flag = true;
 
-  clients.forEach(v=>{ 
-    if(v.id==id){ 
-      flag= false; 
+  clients.forEach(v => {
+    if (v.id == id) {
+      flag = false;
     }
   })
 
-  if(flag) clients.push(temp); 
-  console.log(clients); 
-  
- res.json({}); 
+  if (flag) clients.push(temp);
+  console.log(clients);
+
+  res.json({});
 }
 
 
-let send = async (req,res)=>{
-  let id =req.body.id;
-  let consultant_id;  
-  clients.forEach(v=>{ 
-    if(v.id == id){ 
-      consultant_id = v.consultant; 
+let send = async (req, res) => {
+  let id = req.body.id;
+  let consultant_id;
+  clients.forEach(v => {
+    if (v.id == id) {
+      consultant_id = v.consultant;
     }
   })
- console.log(consultant_id); 
-  res.json({consultant_id,})
+  res.json({ consultant_id, })
 
 }
-
 
 
 
@@ -58,5 +55,5 @@ let send = async (req,res)=>{
 module.exports = {
   request,
   getchat,
-  send, 
+  send,
 }
