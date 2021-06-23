7fd5fe18bcd8c8fff118cb4a7bdee0ef0f28c6be
ehdcjf <kdch0823@naver.com>
2021-06-24 07:27:29 +0900

    teacher update

--- routers/chat/index.js
+++ routers/chat/index.js
@@ -5,19 +5,19 @@ const multer = require('multer'); //npm install multer
 
 
 const upload = multer({
-  storage:multer.diskStorage({
-      destination:function(req,file,callback){
-          callback(null,'uploads')//폴더명
-      },
-      filename:function(req,file,callback){
-          callback(null,new Date().valueOf()+ path.extname(file.originalname))
-      } //path.extname(file.originalname)): 확장자 가져오는 코드
-  }),
+    storage: multer.diskStorage({
+        destination: function (req, file, callback) {
+            callback(null, 'uploads')//폴더명
+        },
+        filename: function (req, file, callback) {
+            callback(null, new Date().valueOf() + path.extname(file.originalname))
+        } //path.extname(file.originalname)): 확장자 가져오는 코드
+    }),
 })
 
-router.get('/', controller.getchat); 
-router.post('/request', controller.request); 
-router.post('/end', upload.single('data'),controller.end);
+router.get('/', controller.getchat);
+router.post('/request', controller.request);
+router.post('/end', upload.single('data'), controller.end);
 router.post('/send', controller.send)
 
 // router.get('/get', controller.get_message)
