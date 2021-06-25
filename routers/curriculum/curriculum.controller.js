1a80e00401b17646bd04ff8758f8199ca86451dd
ehdcjf <kdch0823@naver.com>
2021-06-25 16:19:27 +0900

    curr_head update

--- routers/curriculum/curriculum.controller.js
+++ routers/curriculum/curriculum.controller.js
@@ -54,6 +54,10 @@ let curriculum_view = async (req, res) => {
         if(p==5) p=1; 
     });
 
+    console.log(userid);
+    console.log(username);
+    console.log(nickname);
+
     res.render('./curriculum/curriculum_vie', {
         userid,
         username,
