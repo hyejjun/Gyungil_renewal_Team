9f6f71cf27c052d20b0e7fbf789747df265f0323
simbian2 <simbianartist@gmail.com>
2021-10-21 09:43:36 +0900

    component file명 tsx로 변경했습니다

--- front/client/pages/components/ItemList.tsx
+++ front/client/pages/components/ItemList.tsx
@@ -1,6 +1,9 @@
 import Styled from 'styled-components'
-
+import { useState } from 'react'
 const ItemList = ()=>{
+
+    const [btn, setBtn] = useState<number>(0);
+
     return(
         <>
         </>
