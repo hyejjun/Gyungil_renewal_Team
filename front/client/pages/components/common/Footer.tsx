5fed53ec7c0d3d315e9c623f2867d4db2a8899f3
algml <algml9603@gmail.com>
2021-10-21 11:16:59 +0900

    to merge

--- front/client/pages/components/common/Footer.tsx
+++ front/client/pages/components/common/Footer.tsx
@@ -1,11 +1,27 @@
-import Styled from 'styled-components'
+import Link from 'next/link';
+import Styled from 'styled-components';
 
 const Footer = () => {
     return(
-        <div>
-            footer
-        </div>
+        <FooterWrapper>
+            <div>
+                <p>Azit Gallery</p>
+                <ul>
+                    <li><Link href=""><a>Send FeedBack</a></Link></li>
+                    <li><Link href=""><a>contect us</a></Link></li>
+                    <li><Link href=""><a>english</a></Link></li>
+                </ul>
+            </div>
+            <div></div>
+        </FooterWrapper>
     )
 }
 
 export default Footer
+
+const FooterWrapper = Styled.div`
+    width:100%;
+    height:50px;
+    background-color:#141e28;
+    box-sizing:border-box;   
+`
--- front/client/pages/index.tsx
+++ front/client/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Head from "next/head";
+import Footer from "./components/common/Footer.tsx"
 
 export default function Home() {
   const [text, setText] = useState<string>("자바스크립트");
@@ -14,6 +15,7 @@ export default function Home() {
       <div>
         <span>{text} 적용 완료</span>
       </div>
+      <Footer/>
     </div>
   );
 }
