1c6f902be4ffb241714a3bb2a7774014e1f47939
simbian2 <simbianartist@gmail.com>
2021-10-28 15:16:56 +0900

    id값을 줘서 리스트에 있는 아이템들 각각 번호 임시로 매겼습니다

--- front/client/pages/auction/[auctionview].tsx
+++ front/client/pages/auction/[auctionview].tsx
@@ -1,8 +1,11 @@
 import BackBtn from "../../components/common/BackBtn"
 import NFTPic from "../../components/view/NFTPic"
 import NFTAuction from "../../components/view/auction/NFTAuction"
+import {useRouter} from 'next/router'
 
 const AuctionView = () =>{
+    const router = useRouter()
+    const {view} = router.query // 카테고리 이름
     return(
         <>
             <BackBtn/>
